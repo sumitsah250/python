bb6f5b6968ea53804112e7bafa9b125477d1d2dc
sumit <sumitsah250@gmail.com>
2024-05-11 16:08:17 +0545

    some pratice question

---
+++ test3.py
@@ -0,0 +1,26 @@
+# movies = []
+# movies.append(input("enter the name of first movie"))
+# movies.append(input("enter the name of second movie"))
+# movies.append(input("enter the name of third movie"))
+# print (movies)
+
+# movies1 = []
+# movies1.append(input("enter the first element"))
+# movies1.append(input("enter the second element"))
+# movies1.append(input("enter the third element"))
+# movies2=movies1.copy()
+# movies1.reverse()
+# if(movies1 == movies2):
+#     print("this is palindrome")
+# else:
+#     print("this is not palindrome")
+
+grades=[]
+grades.append(input("enter the first grade:"))
+grades.append(input("enter the second grade:"))
+grades.append(input("enter the third grade:"))
+grades.append(input("enter the forth grade:"))
+grades.append(input("enter the fifth grade:"))
+
+print(grades.count("A"))
+    
