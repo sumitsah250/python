10d53553a6abf7a1e161a2faa81a344bd0325ee2
sumit <sumitsah250@gmail.com>
2024-05-16 17:32:53 +0545

    partten

---
+++ q13_partten_using_loop.py
@@ -0,0 +1,15 @@
+#    *
+#   ***
+#  *****   
+#printing this partten
+num=int(input("enter the size of partten"))
+
+for i in range(1,num+1):
+    for j in range(num-i):
+        print(end=" ")
+    for k in range(i):
+        print("*",end="")
+    for l in range(i-1):
+        print("*",end="")
+    
+    print()
