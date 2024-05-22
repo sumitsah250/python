57e9fcd31ec631b698ada8c33561496e41012f13
sumit <sumitsah250@gmail.com>
2024-05-22 17:30:27 +0545

    pattern

--- numpy/pratice.py
+++ numpy/pratice.py
@@ -15,5 +15,34 @@ b[1,1]=9
 a[1:4,1:4]=b
 # a[1:-1,1:-1]=b # can be done this way too
 
-
 print(a)
+
+#tip
+
+# how to prevent the new copying variable pointing to the same address as that of original
+#let's say
+
+c=np.array([1,2,3,4])
+d=c
+d[1]=909
+print(d)
+print(c)
+
+# output
+# [  1 909   3   4]
+# [  1 909   3   4]
+
+# here the value of both the array changes regardless of changing in one
+# so insted of this we use
+
+e=np.array([1,2,3,4])
+f=e.copy()
+f[0]=99
+print(e)
+print(f)
+ 
+#  output
+# [1 2 3 4]
+# [99  2  3  4]
+
+#here we can see the output where only changed in f and not altur the values of e
